d684377060c78655c6087b3a08ed853c11b6e3d5
7i3k <chenyuliu3@cmail.carleton.ca>
2026-03-22 21:15:05 -0400

    Commit updated files

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,43 @@
+import os
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  # Vite default port
+    "http://localhost:3000",  # React default port
+    "http://localhost:5500",
+    "http://localhost:8001"   # port used for this project
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # allow all HTTP method (GET, POST, etc)
+    allow_headers=["*"],  # alllow all request header
+)
+
+# 1. Simulate an API interface for calling JS
+@app.get("/api/data")
+async def get_data():
+    return {"message": "This is data from FastAPI backend！"}
+
+# 2. Mount static folder so that web browser can access .js file
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+assets_path = os.path.join(BASE_DIR, "assets")
+app.mount("/assets", StaticFiles(directory=assets_path), name="assets")
+
+# 3. Set route for first page, and return to index.html
+@app.get("/")
+async def read_index():
+    index_path = os.path.join(BASE_DIR, "index.html")
+    return FileResponse(index_path)
+
 
 class Question(BaseModel):
     message: str
@@ -62,7 +98,7 @@ Phone Number
 Then click Submit."""}
 
 
-    if "certificate of enrolment" in q or "enrolment" in q:
+    if "certificate of enrollment" in q or "enrollment" in q:
         return {"answer": """To request a certificate of enrolment:
 
 1. Visit Carleton Central
